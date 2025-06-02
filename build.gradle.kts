3aa2b39553b62a0e2d3e7b98230a56ed868e664c
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-06-02 11:50:46 +0200

    upgrade to kotlin to 1.9.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.25"
     id("java")
     id("maven-publish")
 }
